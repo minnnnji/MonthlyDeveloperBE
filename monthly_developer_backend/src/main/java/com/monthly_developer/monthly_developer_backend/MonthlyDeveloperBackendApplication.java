82c61fab9370d17f60775f969b72aa1d6b7db89a
NongE <45160294+NongE@users.noreply.github.com>
2021-09-14 23:33:10 +0900

    Monthly Developer 초기 환경 설정
    - Spring Boot, Java 11, Gradle 환경으로 설정
    - gitignore 추가

---
+++ monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/MonthlyDeveloperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monthly_developer.monthly_developer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyDeveloperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyDeveloperBackendApplication.class, args);
+	}
+
+}
---
+++ monthly_developer_backend/src/test/java/com/monthly_developer/monthly_developer_backend/MonthlyDeveloperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monthly_developer.monthly_developer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyDeveloperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
