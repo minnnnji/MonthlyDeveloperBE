71a372db7123b14d6181a6aece2c149640472d45
NongE <45160294+NongE@users.noreply.github.com>
2021-10-06 23:14:27 +0900

    MongoDB Write 테스트
    - MongoDB에 임의 데이터를 추가하는 테스트 진행
    - 이를 위해 RecruitingTeamPost에 Builder 어노테이션 추가

--- monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/controller/TestController.java
+++ monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.monthly_developer.monthly_developer_backend.controller;
 
+import com.monthly_developer.monthly_developer_backend.model.post.RecruitingTeamPost;
+import com.monthly_developer.monthly_developer_backend.model.user.User;
 import com.monthly_developer.monthly_developer_backend.repository.RecruitingTeamPostRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,16 @@ public class TestController {
     @GetMapping(value = "/mongodb/findall")
     public void mongoDBFindAll(){
         System.out.println(recruitingTeamPostRepository.findAll());
+    }
+
+    @GetMapping(value = "/mongodb/write")
+    public void mongoDBWrite(){
+        RecruitingTeamPost recruitingTeamPost = RecruitingTeamPost.builder()
+                .id("write")
+                .title("write test")
+                .build();
+
+        recruitingTeamPostRepository.save(recruitingTeamPost);
     }
 
 }
--- monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/model/post/RecruitingTeamPost.java
+++ monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/model/post/RecruitingTeamPost.java
@@ -1,10 +1,12 @@
 package com.monthly_developer.monthly_developer_backend.model.post;
 
+import lombok.Builder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 
 @Document(collection = "post")
+@Builder
 public class RecruitingTeamPost {
     @Id
     private String id;
