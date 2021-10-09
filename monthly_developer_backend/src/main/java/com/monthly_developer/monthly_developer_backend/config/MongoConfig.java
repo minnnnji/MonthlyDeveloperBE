c7286a500361c2653faf5382f7a012b66d665904
NongE <hsb9504@gmail.com>
2021-10-09 21:17:42 +0900

    MongoDB 내에 데이터 추가 시 의도하지 않은 필드가 추가되는 문제 수정
    - DefaultMongoTypeMapper를 활용하여 데이터 추가 시 _class 필드 가 추가되지 않도록 수정

---
+++ monthly_developer_backend/src/main/java/com/monthly_developer/monthly_developer_backend/config/MongoConfig.java
@@ -0,0 +1,21 @@
+package com.monthly_developer.monthly_developer_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory, MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter mappingMongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return mappingMongoConverter;
+    }
+}
