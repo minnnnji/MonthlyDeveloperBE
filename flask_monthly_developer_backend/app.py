77d5d8b6f8b121ef0f719d56b0efb14b9216bc74
엄민지 <99x04@naver.com>
2021-11-03 14:32:30 +0900

    글 작성_tags  + 주석 추가

--- flask_monthly_developer_backend/app.py
+++ flask_monthly_developer_backend/app.py
@@ -11,6 +11,7 @@ def create_env():
     app = Flask(__name__)
 
     app.config['MONGO_URI'] = "mongodb://localhost:27017/monthly_developer_db"
+    app.config['JSON_AS_ASCII'] = False
     db_connector.mongo.init_app(app)
 
     api = Api(app, version=1.0, title="flask_env", description="flask_env_test")
--- flask_monthly_developer_backend/controller/hello.py
+++ flask_monthly_developer_backend/controller/hello.py
@@ -9,6 +9,7 @@ introduce = NewPost.model('Introduce', {
     'recruit_title': fields.String(description='recruit title', required=True),
     'recruit_author': fields.String(description='recruit author', required=True),
     'recruit_contents': fields.String(description='recruit contents', required=True),
+    'recruit_tags': fields.List(fields.String, description='recruit tags', required=False),
     'recruit_state': fields.String(description='recruit state', required=True),
 })
 
@@ -17,14 +18,14 @@ introduce = NewPost.model('Introduce', {
 class RecruitPost(Resource):
     @NewPost.expect(introduce)
     def post(self):
-        board = []
+        # 새 글 생성
+        recruit_title = request.json.get("recruit_title") # 제목
+        recruit_author = request.json.get("recruit_author") # 글쓴이
+        recruit_contents = request.json.get('recruit_contents') # 내용
+        recruit_tags = request.json.get('recruit_tags') # tags
+        recruit_state = request.json.get('recruit_state') # 상태
 
-        recruit_title = request.json.get("recruit_title")
-        recruit_author = request.json.get("recruit_author")
-        recruit_contents = request.json.get('recruit_contents')
-        recruit_state = request.json.get('recruit_state')
+        newpost_recruit = {'recruit_title': recruit_title, 'recruit_author': recruit_author,
+                           'recruit_contents': recruit_contents, 'recruit_tags': recruit_tags, 'recruit_state': recruit_state}
 
-        board.append([recruit_title, recruit_author, recruit_contents, recruit_state])
-        print(board)
-        return json.dumps({"status": 200,
-                           "result": {"id": len(board)}})
+        return json.dumps(newpost_recruit, ensure_ascii=False)
