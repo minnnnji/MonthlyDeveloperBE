0547d418a8fe55951389a699e741f6325ae18cee
NongE <hsb9504@gmail.com>
2021-11-03 22:50:54 +0900

    MongoDB 연결관련 테스트 진행
    dump 대신 jsonify을 활용, 반환 형식 수정

--- flask_monthly_developer_backend/app.py
+++ flask_monthly_developer_backend/app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_restx import Api
 from controller import hello
-from controller import connent_test
+from controller import connector
 
+from config import config
 from config import db_connector
 
 
 def create_env():
     app = Flask(__name__)
-    app.config['MONGO_URI'] = "mongodb://localhost:27017/monthly_developer_db"
+    
+    # MongoDB 연결을 설정합니다.
+    app.config['MONGO_URI'] = config.MONGO_URI
     db_connector.mongo.init_app(app)
 
     api = Api(app, version = 1.0, title = "flask_env", description = "flask_env_test")
+
+    # namespace를 추가합니다.
     api.add_namespace(hello.Hello, '/hello')
-    api.add_namespace(connent_test.db_ns, '/db_test')
+    api.add_namespace(connector.db_ns, '/db_test')
 
     return app
