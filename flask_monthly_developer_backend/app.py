ffcf30a4056ff00332036815f49aab24f29c1773
minnnnji <99x04@naver.com>
2021-12-14 21:52:00 +0900

    외부 라이브러리를 사용하지 않은 Github Oauth 구현

--- flask_monthly_developer_backend/app.py
+++ flask_monthly_developer_backend/app.py
@@ -3,8 +3,7 @@ from flask_restx import Api
 
 import os
 
-from controller import recruit_board
-from controller.authentication import oauth, auth
+from controller import recruit_board, authentication
 from config import config
 from config import db_connector
 
@@ -18,12 +17,11 @@ def create_env():
     app.config['SECRET_KEY'] = os.urandom(24)
     db_connector.mongo.init_app(app)
 
-    oauth.init_app(app)
 
     api = Api(app, version=1.0, title="flask_env", description="flask_env_test")
-    app.register_blueprint(auth, url_prefix='/')
 
     # namespace를 추가합니다.
     api.add_namespace(recruit_board.recruit_ns, '/recruit')
+    api.add_namespace(authentication.auth_ns, '/login')
 
     return app
